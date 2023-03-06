affd48dbb1962a8553bfa9d3f95a3a53f7b80e87
MatteoDuppla <mmurcia@nuestro.casa>
2023-03-06 17:02:41 -0500

    test 2

--- src/app.js
+++ src/app.js
@@ -2,7 +2,23 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
+const methodOverride = require('method-override');
+const cors = require('cors');
+const session = require("express-session");
+
 const app = express();
+
+app.use(cors())
+//app.use(cookies())
+app.use(session({
+    secret: "Esto es un secreto",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get("/", (req,res)=>{
     res.send("hola mundo");
